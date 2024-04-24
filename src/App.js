54ea40a1adc30cb09be4a127e0daad55777fa96b
Your Name <nakuldesigner77@gmail.com>
2024-04-25 02:05:20 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Cart from "./components/Cart"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path = "/cart" Component={Cart}/>
+          <Route path = "/" Component={Home}/>
+        </Routes>
+
+
+
+
+      </BrowserRouter>
     </div>
   );
 }
