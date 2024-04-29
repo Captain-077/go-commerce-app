e42d567c3372d078c068b4bd33f73e47575caf3e
Your Name <nakuldesigner77@gmail.com>
2024-04-30 02:26:00 +0530

    backend API,cloudinary added

---
+++ Backend/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id: 1,
+        name: "Iphone 12 pro",
+        desc: "5.6-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714423678/online-shop/iPhone_15_Pink_PDP_Image_Position-1__en-IN_823x_uh8atu.webp"
+    },
+
+    {
+        id: 2,
+        name: "Iphone X pro",
+        desc: "6.1-inch display",
+        price: 799,
+        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714423664/online-shop/images_vskvty.jpg"
+    }, 
+    
+    {
+        id: 3,
+        name: "Iphone 11 pro",
+        desc: "6.5-inch display",
+        price: 899,
+        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714423664/online-shop/images_vskvty.jpg"
+    },
+
+];
+module.exports = products;
---
+++ Frontend/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='Not-found'>
+
+      <h2 className='error'> 404 </h2>
+      <p className="para">Page Not found</p>
+
+
+    </div>
+  )
+}
+
+export default NotFound
