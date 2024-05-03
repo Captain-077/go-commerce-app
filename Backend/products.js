46f8f89d381f8d9c14650b1484b052c9cefeb15a
Your Name <nakuldesigner77@gmail.com>
2024-05-04 02:15:37 +0530

    products displayed with RTK query method and styling starte

--- Backend/products.js
+++ Backend/products.js
@@ -1,26 +1,29 @@
 const products = [
     {
         id: 1,
-        name: "Iphone 12 pro",
+        name: "Iphone 14 plus",
         desc: "5.6-inch display",
         price: 999,
-        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714423678/online-shop/iPhone_15_Pink_PDP_Image_Position-1__en-IN_823x_uh8atu.webp"
+        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714768266/online-shop/iphone14plus/3_v2oq8w.webp",
+        image2: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714768264/online-shop/iphone14plus/1_kl6wjq.webp",
+        image3: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714768265/online-shop/iphone14plus/2_gqilcw.webp",
+
     },
 
     {
         id: 2,
-        name: "Iphone X pro",
+        name: "Samsung s24 ultra",
         desc: "6.1-inch display",
         price: 799,
-        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714423664/online-shop/images_vskvty.jpg"
+        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714768258/online-shop/samsungS24/1_vlcsiq.jpg"
     }, 
     
     {
         id: 3,
-        name: "Iphone 11 pro",
+        name: "Google pixel 6a",
         desc: "6.5-inch display",
         price: 899,
-        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714423664/online-shop/images_vskvty.jpg"
+        image: "https://res.cloudinary.com/dvdu7gdan/image/upload/v1714768263/online-shop/googlePixel6a/3_kcmlla.webp"
     },
 
 ];
--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
 import { useGetAllProductsQuery } from '../Features/ProductsAPI'
 
 function Home() {
-  const {data,error,isLoading} = useGetAllProductsQuery()
+  const { data, error, isLoading } = useGetAllProductsQuery()
+  // const {items,status} = useSelector(state => state.products) <--used with Async thunk-->
   return (
     <>
-    <div>Home</div>
-   
+      <div className="home-container">
+        {isLoading ? <p>Loading...</p> : error ? <p>An Error Occured...</p> :
+
+          <>
+            <h2>New Arrivals</h2>
+
+            <div className="products-section">
+              {data?.map((product) => {
+                return (
+                <div key={product.id} className='product-item'>
+                  <img src={product.image} alt={product.name} />
+                  <h3>{product.name}</h3>
+                  <div className="details">
+                    <span>{product.desc}</span>
+                    <span className='price'>${product.price}</span>
+                  </div>
+                  <button>Add to cart</button>
+                </div>
+                )
+              })}
+
+
+            </div>
+          </>
+
+        }
+      </div>
+
     </>
   )
 }
